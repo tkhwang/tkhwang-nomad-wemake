4e66bdb5a85c50816e78ad2a83a85ed03cf96c2b
tkhwang <tkhwang@gmail.com>
2025-02-02 18:38:50 +0900

    ✨ feat(page/daily-leaderboard): add error handling and param validation
    - add schema validation for year, month, and day params
    - handle future date and invalid date errors
    - display appropriate error message with error code for load errors
    - add ErrorBoundary component for handling and displaying errors
    - update import paths

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,29 +1,55 @@
-import type { Route } from "~/types";
-import type { MetaFunction } from "@remix-run/react";
+import type { Route } from ".react-router/types/app/features/products/pages/+types/daily-leaderboard-page";
+import { DateTime } from "luxon";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
 
-interface DailyLeaderboardPageProps extends Route.ComponentProps {}
-
-export function meta(): MetaFunction {
-  return [
-    { title: "Daily Leaderboard | Product Hunt Clone" },
-    { name: "description", content: "Top products of the day" },
-  ];
-}
+const paramSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
 
 export function loader({ params }: Route.LoaderArgs) {
-  const { year, month, day } = params;
+  const { success, data: parsedData } = paramSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "invalid_params",
+        message: "Invalid params",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  const { year, month, day } = parsedData;
+
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+
+  if (!date.isValid) {
+    throw new Error("Invalid date");
+  }
+
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+
+  if (date > today) {
+    throw data(
+      {
+        error_code: "future_date",
+        message: "Future date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
 
   return {
-    year,
-    month,
-    day,
-    products: [],
+    date,
   };
 }
 
-export default function DailyLeaderboardPage({
-  loaderData,
-}: DailyLeaderboardPageProps) {
+export default function DailyLeaderboardPage({ loaderData }) {
   const { year, month, day, products } = loaderData;
 
   return (
@@ -36,4 +62,20 @@ export default function DailyLeaderboardPage({
       </div>
     </main>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} | {error.data.error_code}
+      </div>
+    );
+  }
+
+  if (error instanceof Error) {
+    return <div>Error in leaderboard: {error.message}</div>;
+  }
+
+  return <div>Unknown Error in leaderboard</div>;
 }
