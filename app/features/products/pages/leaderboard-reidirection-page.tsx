e641f4429edb885328a6b0f05056eb0fbce19f8c
tkhwang <tkhwang@gmail.com>
2025-02-02 14:13:56 +0900

    ✨ feat(page/products): add leaderboard redirection page
    - add support for daily, weekly, monthly, and yearly periods
    - handle invalid period requests with 400 status response

---
+++ app/features/products/pages/leaderboard-reidirection-page.tsx
@@ -0,0 +1,23 @@
+import type { Route } from ".react-router/types/app/features/products/pages/+types/leaderboard-reidirection-page";
+import { data, redirect } from "react-router";
+import { DateTime } from "luxon";
+export function loader({ params, request }: Route.LoaderArgs) {
+  const { period } = params;
+
+  let url: string;
+  const today = DateTime.now().setZone("Asia/Seoul");
+
+  if (period === "daily") {
+    url = `/products/leaderboards/daily/${today.year}/${today.month}/${today.day}`;
+  } else if (period === "weekly") {
+    url = `/products/leaderboards/weekly/${today.year}/${today.weekNumber}`;
+  } else if (period === "monthly") {
+    url = `/products/leaderboards/monthly/${today.year}/${today.month}`;
+  } else if (period === "yearly") {
+    url = `/products/leaderboards/yearly/${today.year}`;
+  } else {
+    return data(null, { status: 400 });
+  }
+
+  return redirect(url);
+}
