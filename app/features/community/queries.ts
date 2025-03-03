e589472319ca6d16033973ca5b9d6871105d17df
tkhwang <tkhwang@gmail.com>
2025-03-03 14:26:39 +0900

    ✨ feat(community/loading): introduce data loading optimization and suspense
    - refactor data loading of community page to use Promise.all for parallel fetching
    - use Suspense component from react for a loading state
    - introduce Await component for response handling
    - add artificial delay in getTopics and getPosts queries for testing suspense feature

--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -43,12 +43,14 @@ import client from "~/features/community/supa-client";
 // };
 
 export const getTopics = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client.from("topics").select("name, slug");
   if (error) throw new Error(error.message);
   return data;
 };
 
 export const getPosts = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client
     .from("community_post_list_view")
     .select(`*`);
